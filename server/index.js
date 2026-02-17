f791f240e94bbeddff916bf371f655d8611af6f6
jbhallay56-max <jbhallay56@gmail.com>
2026-02-17 23:04:32 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import { meRouter } from "./routes/me.js";
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
